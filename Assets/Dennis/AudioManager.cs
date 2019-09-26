d9c4689cbf513a1b03f86a5697976080e2fd2c65
DennisBorst <dennis.borst@hotmail.nl>
2019-09-26 17:45:57 +0200

    Added a script that map the controller buttons

---
+++ Assets/Dennis/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    //The higher the number the angrier your mate is, the lower the number the happier your mate is
+    [Range(-100, 100)]
+    [SerializeField] private int currentAngryMeter;
+
+    [System.Serializable]
+    public class Choises
+    {
+        public AudioClip conversationStarter;
+
+        public AudioClip[] answers;
+        public int[] increaseAngryMeter;
+
+        public AudioClip[] reaction1;
+        public AudioClip[] reaction2;
+        public AudioClip[] reaction3;
+        public AudioClip[] reaction4;
+    }
+
+    [SerializeField] private Choises[] choises;
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void NextSentences()
+    {
+
+    }
+}
---
+++ Assets/Dennis/InputController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private AudioManager audioManager;
+
+    private KeyCode happyEmotion;
+    private KeyCode angryEmotion;
+    private KeyCode neutralEmotion;
+    private KeyCode nervousEmotion;
+
+    void Start()
+    {
+        ConfigureControlButtons();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void ConfigureControlButtons()
+    {
+        //here are the emotions mapped on the controller
+
+        //A button
+        happyEmotion = KeyCode.Joystick1Button0;
+
+        //B button
+        angryEmotion = KeyCode.Joystick1Button1;
+
+        //X button
+        neutralEmotion = KeyCode.Joystick1Button2;
+
+        //Y button
+        nervousEmotion = KeyCode.Joystick1Button3;
+    }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(happyEmotion))
+        {
+            Debug.Log("I am happy");
+        }
+        else if (Input.GetKeyDown(angryEmotion))
+        {
+            Debug.Log("I am angry");
+        }
+        else if (Input.GetKeyDown(neutralEmotion))
+        {
+            Debug.Log("I am neutral");
+        }
+        else if (Input.GetKeyDown(nervousEmotion))
+        {
+            Debug.Log("I am nervous");
+        }
+    }
+}
